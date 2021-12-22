ebf3df16f211976b9abe05c0e2ee28dbeaca1721
david <david.tamayo.m@hotmail.com>
2021-12-22 18:30:28 -0500

    Navegacion entre actividades con los botones del home

---
+++ app/src/main/java/com/example/logiejecutivos/filtro/FiltroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.logiejecutivos.filtro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.logiejecutivos.R
+
+class FiltroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filtro)
+    }
+}
